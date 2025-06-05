ec2d58f66291b42f316be123970b5a61cea076b1
MrMaxie <mxmsyuu@gmail.com>
2025-06-05 19:11:56 +0200

    feat(gallery): replace react-grid-gallery with justified-layout for improved image layout

--- client/src/pages/Gallery.tsx
+++ client/src/pages/Gallery.tsx
@@ -1,71 +1,138 @@
-import { Gallery as GridGallery, type Image } from 'react-grid-gallery';
+import { Image } from '@mantine/core';
 import Lightbox from 'react-image-lightbox';
+import { useEffect, useRef, useState } from 'react';
 import 'react-image-lightbox/style.css';
-import { useEffect, useState } from 'react';
+import justifiedLayout from 'justified-layout';
 
-type PicsumPhoto = {
+type ImageType = {
     id: string;
     author: string;
     width: number;
     height: number;
-    download_url: string;
+    downloadUrl: string;
+    caption: string;
+    src: string;
+    original: string;
 };
 
-type GalleryImage = Image & { original: string };
-
 export const Gallery = () => {
-    const [images, setImages] = useState<GalleryImage[]>([]);
+    const containerRef = useRef<HTMLDivElement>(null);
+    const [images, setImages] = useState<ImageType[]>([]);
     const [index, setIndex] = useState(-1);
+    const [containerWidth, setContainerWidth] = useState(0);
 
     useEffect(() => {
         const fetchImages = async () => {
-            try {
-                const response = await fetch(
-                    'https://picsum.photos/v2/list?page=1&limit=25',
-                );
-                const data: PicsumPhoto[] = await response.json();
-                const imgs: GalleryImage[] = data.map(item => ({
-                    src: `https://picsum.photos/id/${item.id}/300/200`,
-                    original: item.download_url,
-                    width: 300,
-                    height: 200,
+            const response = await fetch('https://picsum.photos/v2/list?page=1&limit=25');
+            const data = await response.json();
+            const possibleSizes = [
+                [400, 400],
+                [600, 400],
+                [800, 600],
+                [1200, 800],
+            ];
+
+            const imgs: ImageType[] = data.map((item: {
+                id: string;
+                author: string;
+                width: number;
+                height: number;
+                download_url: string;
+            }) => {
+                const [w, h] = possibleSizes[Math.floor(Math.random() * possibleSizes.length)];
+
+                return {
+                    id: item.id,
+                    author: item.author,
+                    width: w,
+                    height: h,
+                    downloadUrl: item.download_url,
                     caption: item.author,
-                }));
-                setImages(imgs);
-            } catch (error) {
-                console.error('Failed to load images', error);
-            }
+                    src: `https://picsum.photos/id/${item.id}/${w}/${h}`,
+                    original: item.download_url,
+                };
+            });
+            setImages(imgs);
         };
 
         fetchImages();
     }, []);
 
+    // 📏 Zmierz szerokość kontenera (responsive)
+    useEffect(() => {
+        const resizeObserver = new ResizeObserver((entries) => {
+            for (let entry of entries) {
+                if (entry.contentRect.width) {
+                    setContainerWidth(entry.contentRect.width);
+                }
+            }
+        });
+
+        if (containerRef.current) {
+            resizeObserver.observe(containerRef.current);
+        }
+
+        return () => resizeObserver.disconnect();
+    }, []);
+
     const currentImage = images[index];
     const nextIndex = (index + 1) % images.length;
     const prevIndex = (index + images.length - 1) % images.length;
-    const nextImage = images[nextIndex] || currentImage;
-    const prevImage = images[prevIndex] || currentImage;
 
-    const handleClick = (i: number) => setIndex(i);
-    const handleClose = () => setIndex(-1);
-    const handleMovePrev = () => setIndex(prevIndex);
-    const handleMoveNext = () => setIndex(nextIndex);
+    // 🧮 Oblicz layout dopiero, gdy mamy szerokość i obrazki
+    const layout =
+        containerWidth && images.length
+            ? justifiedLayout(
+                    images.map((img) => img.width / img.height), // aspect ratios
+                    {
+                        containerWidth,
+                        targetRowHeight: 200,
+                        boxSpacing: 6,
+                    }
+                )
+            : null;
 
     return (
-        <div>
-            <GridGallery
-                images={images}
-                onClick={handleClick}
-                enableImageSelection={false}
-            />
-            {!!currentImage && (
+        <div ref={containerRef} style={{ position: 'relative', width: '100%' }}>
+            {layout &&
+                layout.boxes.map((box, i) => (
+                    <div
+                        key={images[i].id}
+                        onClick={() => setIndex(i)}
+                        style={{
+                            position: 'absolute',
+                            top: box.top,
+                            left: box.left,
+                            width: box.width,
+                            height: box.height,
+                            cursor: 'pointer',
+                            background: '#eee',
+                            overflow: 'hidden',
+                        }}
+                    >
+                        <Image
+                            src={images[i].src}
+                            alt={images[i].caption}
+                            fit="contain"
+                            width="100%"
+                            height="100%"
+                            style={{ display: 'block' }}
+                        />
+                    </div>
+                ))}
+
+            {!!layout && (
+                <div style={{ height: layout.containerHeight }} /> // ⬅️ kontener z wysokością
+            )}
+
+            {currentImage && (
                 <Lightbox
                     mainSrc={currentImage.original}
-                    nextSrc={nextImage.original}
-                    prevSrc={prevImage.original}
-                    onCloseRequest={handleClose}
-                    onMovePrevRequest={handleMovePrev}
-                    onMoveNextRequest={handleMoveNext}
+                    nextSrc={images[nextIndex].original}
+                    prevSrc={images[prevIndex].original}
+                    onCloseRequest={() => setIndex(-1)}
+                    onMovePrevRequest={() => setIndex(prevIndex)}
+                    onMoveNextRequest={() => setIndex(nextIndex)}
                 />
             )}
         </div>
